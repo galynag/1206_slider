87aa30a65e710b6c8d156fc277f2a3e6f3063f6d
galynag <g.alenicheva@gmail.com>
2017-06-12 14:03:06 +0300

    v0.2 every thing is good

--- js.js
+++ js.js
@@ -1,38 +1,78 @@
+$(document).ready(function() {
+    var counter=0;
+    var i=0;
+    $('#box').on('wheel',function slider(e) {
+        $('#box div').css('height', e.view.innerHeight + 'px');
+        console.log(e.originalEvent.deltaY);
+        //content down
+        if (e.originalEvent.deltaY < 0) {
+            if (i > 0 && i < $('#box div').length - 1)  {
+                if (counter < 0) {
+                    counter = counter + 10;
+                    $('#box div').eq(i).css('top', counter + 'px');
+                } else {
+                    i--;
+                    counter=-e.view.innerHeight + 10;
+                    $('#box div').eq(i).css('top', counter + 'px');
+                }
+            }
+            else if (i == $('#box div').length - 1)  {
+                counter = -e.view.innerHeight + 10;
+                $('#box div').eq(i-1).css('top', counter + 'px');
+                i = $('#box div').length - 2;
+            }
+               else {
+                if (counter < 0) {
+                    counter = counter + 10;
+                    $('#box div').eq(i).css('top', counter + 'px');
+                } else {
+                    return false;
+                }
+            }
+            //content up
+        } else {
+                if (i < $('#box div').length - 1) {
+                    if (counter > -e.view.innerHeight) {
+                        counter = counter - 10;
+                        $('#box div').eq(i).css('top', counter + 'px');
+                    } else {
+                        counter = 0;
+                        i++;
+                    }
+                } else {
+                    return false;
+                }
+            }
+    });
+});
+
 // $(document).ready(function() {
 //     var counter=0;
 //     var i=0;
 //     $('#box').on('wheel',function slider(e) {
-//         $('#box div').css('height', e.view.innerHeight + 'px');
 //         console.log(e.originalEvent.deltaY);
-//         if (i > 0)  {
-//
 //         if (e.originalEvent.deltaY < 0) {
-//
+//             $('#box div').css('height',e.view.innerHeight+'px');
 //             if (counter < e.view.innerHeight) {
-//                 counter = counter + 10;
-//                 $('#box div').eq(i).css('top', counter + 'px');
-//                 i--;
+//                 counter =counter +10;
+//                 $('#box div').eq(i).css('top',counter+'px');
 //
 //             } else {
-//                 counter = 0;
+//                 counter=0;
+//                 if (i > 0)  {
+//                     i--;
+//                 } else {
+//                     return false;}
 //             }
+//         }
+//         else {
+//             $('#box div').css('height', e.view.innerHeight + 'px');
 //             if (counter > -e.view.innerHeight) {
 //                 counter = counter - 10;
 //                 $('#box div').eq(i).css('top', counter + 'px');
 //
 //             } else {
 //                 counter = 0;
-//             }
-//         }
-//
-//                 else {
-//                     return false;}
-//             }
-//
-//
-//         else {
-//
-//
 //                 if (i < $('#box div').length - 2) {
 //                     i++;
 //                 } else {
@@ -44,43 +84,4 @@
 //     });
 //
 // });
-
-$(document).ready(function() {
-    var counter=0;
-    var i=0;
-    $('#box').on('wheel',function slider(e) {
-        console.log(e.originalEvent.deltaY);
-        if (e.originalEvent.deltaY < 0) {
-            $('#box div').css('height',e.view.innerHeight+'px');
-            if (counter < e.view.innerHeight) {
-                counter =counter +10;
-                $('#box div').eq(i).css('top',counter+'px');
-
-            } else {
-                counter=0;
-                if (i > 0)  {
-                    i--;
-                } else {
-                    return false;}
-            }
-        }
-        else {
-            $('#box div').css('height', e.view.innerHeight + 'px');
-            if (counter > -e.view.innerHeight) {
-                counter = counter - 10;
-                $('#box div').eq(i).css('top', counter + 'px');
-
-            } else {
-                counter = 0;
-                if (i < $('#box div').length - 2) {
-                    i++;
-                } else {
-                    return false;
-                }
-            }
-        }
-
-    });
-
-});
 
